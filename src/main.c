82cd2d19d33d76f71d645df986015ce6e8d8ba13
ultrakaktus <jkozarik@gmail.com>
2016-10-03 15:29:57 +0200

    uloha1commit1
    nastavenie GPIO pinu

---
+++ src/main.c
@@ -0,0 +1,120 @@
+/**
+*****************************************************************************
+**
+**  File        : main.c
+**
+**  Abstract    : main function.
+**
+**  Functions   : main
+**
+**  Environment : Atollic TrueSTUDIO/STM32
+**                STMicroelectronics STM32Lxx Standard Peripherals Library
+**
+**  Distribution: The file is distributed "as is", without any warranty
+**                of any kind.
+**
+**  (c)Copyright Atollic AB.
+**  You may use this file as-is or modify it according to the needs of your
+**  project. This file may only be built (assembled or compiled and linked)
+**  using the Atollic TrueSTUDIO(R) product. The use of this file together
+**  with other tools than Atollic TrueSTUDIO(R) is not permitted.
+**
+*****************************************************************************
+*/
+
+/* Includes */
+#include <stddef.h>
+#include "stm32l1xx.h"
+
+
+/* Private typedef */
+/* Private define  */
+/* Private macro */
+/* Private variables */
+/* Private function prototypes */
+/* Private functions */
+
+
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
+int main(void)
+{
+  int i = 0;
+
+  /**
+  *  IMPORTANT NOTE!
+  *  See the <system_*.c> file and how/if the SystemInit() function updates 
+  *  SCB->VTOR register. Sometimes the symbol VECT_TAB_SRAM needs to be defined 
+  *  when building the project if code has been located to RAM and interrupts 
+  *  are used. Otherwise the interrupt table located in flash will be used.
+  *  E.g.  SCB->VTOR = 0x20000000;  
+  */
+
+  /**
+  *  At this stage the microcontroller clock setting is already configured,
+  *  this is done through SystemInit() function which is called from startup
+  *  file (startup_stm32l1xx_hd.s) before to branch to application main.
+  *  To reconfigure the default setting of SystemInit() function, refer to
+  *  system_stm32l1xx.c file
+  */
+
+  /* TODO - Add your application code here */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* Infinite loop */
+  while (1)
+  {
+	i++;
+  }
+  return 0;
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/*
+ * Minimal __assert_func used by the assert() macro
+ * */
+void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
+{
+  while(1)
+  {}
+}
+
+/*
+ * Minimal __assert() uses __assert__func()
+ * */
+void __assert(const char *file, int line, const char *failedexpr)
+{
+   __assert_func (file, line, NULL, failedexpr);
+}
